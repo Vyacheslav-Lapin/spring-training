95959c4cf71302818d5f8609a1668d031a9901c2
FlaIDzeres <FlaIDzeres@gmail.com>
2017-03-21 16:51:42 +0300

    lab 5 fix tests

--- src/main/java/lab/model/Customer.java
+++ src/main/java/lab/model/Customer.java
@@ -2,11 +2,13 @@ package lab.model;
 
 import lombok.Data;
 import lombok.experimental.Accessors;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Data
 @Accessors(chain = true, fluent = true)
 @Component
+@Scope("prototype")
 public class Customer implements Person {
     private String name;
     private boolean broke;
